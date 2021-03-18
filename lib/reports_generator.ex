230246bd2b9e65621c6df933deaa76f22710a161
Pedro Moreno <nennogabriel@gmail.com>
2021-03-18 14:35:54 -0300

    leson 2

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,10 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, _reason}), do: "Error while opening file!"
 end
