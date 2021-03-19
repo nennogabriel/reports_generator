325a91c95c0aea0fa7a4d2f4a030b49a7a5b43d7
Pedro Moreno <nennogabriel@gmail.com>
2021-03-19 18:53:06 -0300

    lesson 7

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -12,13 +12,19 @@ defmodule ReportsGenerator do
     "sushi"
   ]
 
+  @options ["foods", "users"]
+
   def build(filename) do
     "reports/#{filename}"
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  def fech_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+  def fech_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fech_higher_cost(_report, _option), do: {:error, "invalid option"}
 
   defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
     users = Map.put(users, id, users[id] + price)
