f969a7fe135fa0e303e6b42594839596294a3fc2
Pedro Moreno <nennogabriel@gmail.com>
2021-03-21 23:45:24 -0300

    build_from_any

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,13 +20,26 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_any(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
+  def build_from_any(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
   def fech_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
 
   def fech_higher_cost(_report, _option), do: {:error, "invalid option"}
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
@@ -34,12 +47,27 @@ defmodule ReportsGenerator do
     # |> Map.put("users", users)
     # |> Map.put("foods", foods)
 
-    %{report | "users" => users, "foods" => foods}
+    build_report(foods, users)
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
